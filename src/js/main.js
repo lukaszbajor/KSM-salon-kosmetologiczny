b89a300fff2f5561b56020c78bd7d7dc2c73b8aa
lukaszbajor <48011715+lukaszbajor@users.noreply.github.com>
2024-06-05 00:35:12 +0200

    changing in header, contact and page basic

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ const burger = document.querySelector(".nav__burger");
 const menu = document.querySelector(".nav__list");
 
 const allMenuItems = document.querySelectorAll(".nav__link");
+const allBars = document.querySelectorAll(".page__bar");
 
 const menuBurger = () => {
 	burger.classList.toggle("nav__burger--active");
@@ -16,3 +17,20 @@ allMenuItems.forEach((item) => {
 });
 
 burger.addEventListener("click", menuBurger);
+
+allBars.forEach((bar) => {
+	bar.addEventListener("click", () => {
+		const answer = bar.nextElementSibling;
+		const isOpen = answer.classList.contains("page__answer--open");
+
+		document.querySelectorAll(".page__answer").forEach((item) => {
+			item.classList.remove("page__answer--open");
+			item.style.height = null;
+		});
+
+		if (!isOpen) {
+			answer.classList.add("page__answer--open");
+			// answer.style.height = answer.scrollHeight + "px";
+		}
+	});
+});
