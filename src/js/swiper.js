ea4fd6819ebb1cb61a19d799a418cf7a84144eca
lukaszbajor <48011715+lukaszbajor@users.noreply.github.com>
2024-06-04 16:50:15 +0200

    contact page mobile and desktop

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 	const swiper = new Swiper(".swiper-container", {
 		slidesPerView: 1,
-		spaceBetween: 10,
+		spaceBetween: 20,
 		navigation: {
 			nextEl: ".swiper-button-next",
 			prevEl: ".swiper-button-prev",
@@ -14,12 +14,12 @@ document.addEventListener("DOMContentLoaded", function () {
 			// when window width is >= 576px
 			576: {
 				slidesPerView: 2,
-				spaceBetween: 10,
+				spaceBetween: 25,
 			},
 			// when window width is >= 992px
 			992: {
 				slidesPerView: 3,
-				spaceBetween: 10,
+				spaceBetween: 20,
 			},
 		},
 		autoplay: {
